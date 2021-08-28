bade8dd4dcbcf3ed2c2ec69cb9141ae2ea3ab3d4
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-28 20:26:06 +0300

    added webcam

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ file_name = f"{time_stamp}.mp4"
 fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
 captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
 
+webcam = cv2.VideoCapture(1)
 
 while True:
     img = ImageGrab.grab(bbox=(0, 0, width, height))
     img.np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    _, frame = webcam.read()
     cv2.imshow("Screen_Recorder", img_final)
+
+    cv2.imshow("webcam", frame)
+
     captured_video.write(img_final)
     if cv2.waitKey(10) == ord("q"):
         break
